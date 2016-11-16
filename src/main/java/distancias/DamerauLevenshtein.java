641203fb1c3b9cfb117f8f0d9eff0b8e64fc4c80
MarcellaTS <marcella.tavares@uniriotec.br>
2016-11-16 21:09:15 -0200

    distancias levenshtein e damerau-levenshtein

---
+++ src/main/java/distancias/DamerauLevenshtein.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distancias;
+
+/**
+ *
+ * @author marce
+ */
+public class DamerauLevenshtein {
+    /*Calcula distância entre palavras pelo algoritmo de Damerau-Levenshtein:
+        Essa distância é calculada pelo mínimo de operações para transformar uma palavra na outra.
+        As operações consideradas nesse algoritmo são: inserir, deletar, substituir um caracter ou trocar dois carecteres de lugar.*/
+    public static int computeLevenshteinDistance(String palavra_um, String palavra_dois){
+        char caracter_x;
+        char caracter_y;
+        int custo;
+
+        int tamanho_palavra_um = palavra_um.length ();
+        int tamanho_palavra_dois = palavra_dois.length ();
+        
+        if (tamanho_palavra_um == 0) {
+            return tamanho_palavra_dois;
+        }
+        if (tamanho_palavra_dois == 0) {
+            return tamanho_palavra_um;
+        }
+        
+         int[][] matriz_calcular_distancia = new int[tamanho_palavra_um+1][tamanho_palavra_dois+1];
+
+        for (int x = 0; x <= tamanho_palavra_um; x++) {
+            matriz_calcular_distancia[x][0] = x;
+        }
+
+        for (int y = 0; y <= tamanho_palavra_dois; y++) {
+            matriz_calcular_distancia[0][y] = y;
+        }
+
+        for(int x = 1; x <= tamanho_palavra_um; x++) {
+            caracter_x = palavra_um.charAt (x - 1);
+            for(int y = 1; y <= tamanho_palavra_dois; y++) {
+                caracter_y = palavra_dois.charAt (y - 1);
+
+                if(caracter_x == caracter_y){
+                    custo = 0;
+                }else{
+                    custo = 1;
+                }
+                int deletar = matriz_calcular_distancia[x - 1][y] + 1;
+                int inserir = matriz_calcular_distancia[x][y - 1] + 1;
+                int substituir = matriz_calcular_distancia[x - 1][y - 1] + custo;
+                matriz_calcular_distancia[x][y] = Math.min(deletar, Math.min(inserir, substituir));
+                
+                //Verifica se a distância mínima ocorre a partir de uma das operações acima ou por transposição
+                if(x > 1 && y > 1 && caracter_x == caracter_y-1 && caracter_x-1 == caracter_y){ 
+                    matriz_calcular_distancia[x][y] = Math.min(matriz_calcular_distancia[x][y], matriz_calcular_distancia[x - 2][y - 2] + custo);
+                }
+            }
+        }
+        return matriz_calcular_distancia[tamanho_palavra_um][tamanho_palavra_dois];
+    }
+}
--- src/main/java/distancias/Levenshtein.java
+++ src/main/java/distancias/Levenshtein.java
@@ -10,30 +10,39 @@ package distancias;
  * @author marce
  */
 public class Levenshtein {
- 
-    public static int distance(String a, String b) {
-        a = a.toLowerCase();
-        b = b.toLowerCase();
-        // i == 0
-        int [] costs = new int [b.length() + 1];
-        for (int j = 0; j < costs.length; j++)
-            costs[j] = j;
-        for (int i = 1; i <= a.length(); i++) {
-            // j == 0; nw = lev(i - 1, j)
-            costs[0] = i;
-            int nw = i - 1;
-            for (int j = 1; j <= b.length(); j++) {
-                int cj = Math.min(1 + Math.min(costs[j], costs[j - 1]), a.charAt(i - 1) == b.charAt(j - 1) ? nw : nw + 1);
-                nw = costs[j];
-                costs[j] = cj;
+    /*Calcula distância entre palavras pelo algoritmo de Levenshtein:
+        Essa distância é calculada pelo mínimo de operações para transformar uma palavra na outra.
+        As operações consideradas nesse algoritmo são: inserir, deletar ou substituir um caracter.*/
+    public int DistanciaLevenshtein(String palavra_um, String palavra_dois){
+        char[] array_palavra_um = palavra_um.toUpperCase().toCharArray();
+        char[] array_palavra_dois = palavra_dois.toUpperCase().toCharArray();
+
+        int[][] matriz_calcular_distancia = new int[array_palavra_um.length + 1][array_palavra_dois.length + 1];
+        
+        int custo = 0;
+
+        for (int x = 0; x < array_palavra_um.length + 1; x++) {
+            matriz_calcular_distancia[x][0] = x;
+        }
+        for (int y = 0; y < array_palavra_dois.length + 1; y++) {
+            matriz_calcular_distancia[0][y] = y;
+        }
+
+        for (int x = 1; x < array_palavra_um.length + 1; x++) {
+            for (int y = 1; y < array_palavra_dois.length + 1; y++) {
+                    custo = 1;
+                if(array_palavra_um[x - 1] == array_palavra_dois[y - 1]){
+                    custo = 0;
+                }
+                int deletar = matriz_calcular_distancia[x - 1][y] + 1;
+                int inserir = matriz_calcular_distancia[x][y - 1] + 1;
+                int substituir = matriz_calcular_distancia[x - 1][y - 1] + custo;
+                matriz_calcular_distancia[x][y] = Math.min(deletar, Math.min(inserir, substituir));
             }
         }
-        return costs[b.length()];
-    }
- 
-    public static void main(String [] args) {
-        String [] data = { "kitten", "sitting", "saturday", "sunday", "rosettacode", "raisethysword" };
-        for (int i = 0; i < data.length; i += 2)
-            System.out.println("distance(" + data[i] + ", " + data[i+1] + ") = " + distance(data[i], data[i+1]));
-    }
+
+        int resultado = matriz_calcular_distancia[array_palavra_um.length][array_palavra_dois.length];
+
+        return resultado;
+    }    
 }
